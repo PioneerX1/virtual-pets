e72530014ff65d95d1633aaf4346265ba18ebfff
Michael Dunlap and Mick Sexton <michael@>
2017-07-17 17:03:53 -0700

    add abstract class for Monster and separate Fire and Water classes, along with testing

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -6,34 +6,26 @@ import java.util.Timer;
 import java.util.TimerTask;
 import org.sql2o.*;
 
-public class Monster {
-  private String name;
-  private int personId;
-  private int id;
-  private int foodLevel;
-  private int sleepLevel;
-  private int playLevel;
+public abstract class Monster {
+  public String name;
+  public int personId;
+  public int id;
+  public int foodLevel;
+  public int sleepLevel;
+  public int playLevel;
+  public String type;
 
-  private Timestamp birthday;
-  private Timestamp lastSlept;
-  private Timestamp lastAte;
-  private Timestamp lastPlayed;
-  private Timer timer;
+  public Timestamp birthday;
+  public Timestamp lastSlept;
+  public Timestamp lastAte;
+  public Timestamp lastPlayed;
+  public Timer timer;
 
   public static final int MAX_FOOD_LEVEL = 3;
   public static final int MAX_SLEEP_LEVEL = 8;
   public static final int MAX_PLAY_LEVEL = 12;
   public static final int MIN_ALL_LEVELS = 0;
 
-
-  public Monster(String name, int personId) {
-    this.name = name;
-    this.personId = personId;
-    this.playLevel = MAX_PLAY_LEVEL / 2;
-    this.sleepLevel = MAX_SLEEP_LEVEL / 2;
-    this.foodLevel = MAX_FOOD_LEVEL / 2;
-    timer = new Timer();
-  }
 
   public String getName(){
     return name;
@@ -88,31 +80,16 @@ public class Monster {
 
   public void save() {
     try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO monsters (name, personid, birthday, lastate, lastslept, lastplayed) VALUES (:name, :personId, now(), now(), now(), now())";
+      String sql = "INSERT INTO monsters (name, personid, birthday, lastate, lastslept, lastplayed, type) VALUES (:name, :personId, now(), now(), now(), now(), :type)";
       this.id = (int) con.createQuery(sql, true)
         .addParameter("name", this.name)
         .addParameter("personId", this.personId)
+        .addParameter("type", this.type)
         .executeUpdate()
         .getKey();
     }
   }
 
-  public static List<Monster> all() {
-    String sql = "SELECT * FROM monsters";
-    try(Connection con = DB.sql2o.open()) {
-      return con.createQuery(sql).executeAndFetch(Monster.class);
-    }
-  }
-
-  public static Monster find(int id) {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "SELECT * FROM monsters where id=:id";
-      Monster monster = con.createQuery(sql)
-        .addParameter("id", id)
-        .executeAndFetchFirst(Monster.class);
-      return monster;
-    }
-  }
 
   public boolean isAlive() {
     if (foodLevel <= MIN_ALL_LEVELS || playLevel <= MIN_ALL_LEVELS || sleepLevel <= MIN_ALL_LEVELS) {
